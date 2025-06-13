e688a86245908ec8b297cf61c9db3e40b7e7410f
Heikki Huttu <heissendo88@gmail.com>
2025-06-13 21:12:57 +0300

    add write function

--- dummy.c
+++ dummy.c
@@ -1,3 +1,6 @@
+#include "linux/gfp_types.h"
+#include "linux/slab.h"
+#include "linux/types.h"
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/fs.h>
@@ -8,60 +11,81 @@
 
 static int dummy_open(struct inode *inode, struct file *file)
 {
-    pr_info("dummy: device opened\n");
-    return 0;
+	pr_info("dummy: device opened\n");
+	return 0;
 }
 
 static int dummy_release(struct inode *inode, struct file *file)
 {
-    pr_info("dummy: device closed\n");
-    return 0;
+	pr_info("dummy: device closed\n");
+	return 0;
 }
 
 static ssize_t dummy_read(struct file *file, char __user *buf, size_t len, loff_t *offset)
 {
-    const char *msg = "Hello from dummy driver!\n";
-    size_t msg_len = strlen(msg);
+	const char *msg = "Hello from dummy driver!\n";
+	size_t msg_len = strlen(msg);
 
-    if (*offset >= msg_len)
-        return 0;
+	if (*offset >= msg_len)
+		return 0;
 
-    if (len > msg_len - *offset)
-        len = msg_len - *offset;
+	if (len > msg_len - *offset)
+		len = msg_len - *offset;
 
-    if (copy_to_user(buf, msg + *offset, len))
+	if (copy_to_user(buf, msg + *offset, len))
+		return -EFAULT;
+
+	*offset += len;
+	return len;
+}
+
+static ssize_t dummy_write(struct file *file, const char __user *buf, size_t len, loff_t *offset)
+{
+    char *msg = kmalloc(len + 1, GFP_KERNEL);
+    if (!msg)
+        return -ENOMEM;
+
+    if (copy_from_user(msg, buf, len)) {
+        kfree(msg);
         return -EFAULT;
+    }
+
+    msg[len] = '\0';
+    pr_info("dummy: received: %s\n", msg);
 
     *offset += len;
+
+    kfree(msg);
     return len;
 }
 
 static const struct file_operations dummy_fops = {
-    .owner = THIS_MODULE,
-    .open = dummy_open,
-    .release = dummy_release,
-    .read = dummy_read,
+	.owner = THIS_MODULE,
+	.open = dummy_open,
+	.release = dummy_release,
+	.read = dummy_read,
+	.write = dummy_write,
 };
 
 static int __init dummy_init(void)
 {
-    int ret;
+	int ret;
 
-    ret = register_chrdev(MAJOR_NUM, DEVICE_NAME, &dummy_fops);
-    if (ret < 0) {
-        pr_err("dummy: failed to register device\n");
-        return ret;
-    }
+	ret = register_chrdev(MAJOR_NUM, DEVICE_NAME, &dummy_fops);
+	if (ret < 0) {
+		pr_err("dummy: failed to register device\n");
+		return ret;
+	}
 
-    pr_info("dummy: module loaded\n");
-    pr_info("dummy: create device node with: mknod /dev/%s c %d 0\n", DEVICE_NAME, MAJOR_NUM);
-    return 0;
+	pr_info("dummy: module loaded\n");
+	pr_info("dummy: create device node with: mknod /dev/%s c %d 0\n", DEVICE_NAME, MAJOR_NUM);
+	return 0;
 }
 
 static void __exit dummy_exit(void)
 {
-    unregister_chrdev(MAJOR_NUM, DEVICE_NAME);
-    pr_info("dummy: module unloaded\n");
+	unregister_chrdev(MAJOR_NUM, DEVICE_NAME);
+	pr_info("dummy: module unloaded\n");
 }
 
 module_init(dummy_init);
